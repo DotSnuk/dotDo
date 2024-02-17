5da1d544ffac7e985fc2305d71204ca13ee14711
Hampus Farner <hampusfarner@outlook.com>
2024-02-17 07:53:41 +0100

    Poppins font added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
+import { format, compareAsc } from 'date-fns';
 
 function component() {
   const element = document.createElement('div');
   element.innerText = 'Hello';
+  console.log(format(new Date(2014, 1, 11), 'dd/MM/yyyy'));
   return element;
 }
 
